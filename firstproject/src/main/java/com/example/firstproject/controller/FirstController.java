f8014d36db2488159af3d1f87b27fb9176b72ce8
GomCoder <k2001911@hanmail.net>
2023-12-15 20:57:55 +0900

    2장 MVC 패턴 이해와 실습 - Model 객체를 이용하여 View Template의 변수에 값 적용하기2

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -16,4 +16,15 @@ public class FirstController {
         model.addAttribute("username", "사용자");
         return "greetings";
     }
+
+    /**
+     * URL: localhost:8080/bye GET 요청시
+     * @param model model 객체
+     * @return 받아온 model 객체를 goodbye.mustache의 nickname 변수에 반영 후 반환
+     */
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
 }
