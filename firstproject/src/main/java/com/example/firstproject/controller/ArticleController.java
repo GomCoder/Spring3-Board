b5b6f97538fbbfffac56dcaecccfdda4dacd4481
GomCoder <k2001911@hanmail.net>
2023-12-17 16:35:39 +0900

    5장 게시글 읽기: Read

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -42,5 +46,38 @@ public class ArticleController {
         log.info(saved.toString());
         //System.out.println(saved.toString()); //리포지토리 값 확인
         return "";
+    }
+
+    /**
+     * localhost:8080/articles/{id} GET 요청시 데이터 조회 요청
+     * @param id Atricle의 id
+     * @return
+     */
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id,
+                       Model model) {
+        log.info("id= " +id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    /**
+     * localhost:8080/articles GET 요청시 데이터 목록 조회하기
+     * @return
+     */
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @Slf4j
@@ -32,5 +36,36 @@ public class MemberController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    /**
+     * localhost:8080/members/{id} GET 요청시 데이터 조회 요청
+     * @param id Atricle의 id
+     * @return
+     */
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id,
+                       Model model) {
+        log.info("id= " +id);
+        //1. id를 조회해 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        //3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+
+    /**
+     * localhost:8080/members GET 요청시 데이터 목록 조회하기
+     * @return
+     */
+    @GetMapping("/members")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("memberList", memberEntityList);
+        //3. 뷰 페이지 설정하기
+        return "members/index";
     }
 }
