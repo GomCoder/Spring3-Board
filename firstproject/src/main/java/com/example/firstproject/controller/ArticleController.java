2c0c904daeaaa5de344984ff0ae0232e11797cc9
GomCoder <k2001911@hanmail.net>
2023-12-22 16:45:59 +0900

    8장 게시글 삭제하기: Delete / 9장 CRUD와 SQL 쿼리 종합

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -30,7 +31,7 @@ public class ArticleController {
     /**
      * localhost:8080/articles/create POST 요청시 (Form 입력값)
      * 폼 데이터를 전송받아 DTO 객체에 담은 후 데이터 베이스에 저장한다.
-     * @return
+     * @return localhost:8080/articles/ + 저장된 id
      */
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
@@ -50,8 +51,8 @@ public class ArticleController {
 
     /**
      * localhost:8080/articles/{id} GET 요청시 데이터 조회 요청
-     * @param id Atricle의 id
-     * @return
+     * @param id 해당 atricle의 id
+     * @return localhost:8080/articles/show
      */
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id,
@@ -69,7 +70,7 @@ public class ArticleController {
 
     /**
      * localhost:8080/articles GET 요청시 데이터 목록 조회하기
-     * @return
+     * @return localhost:8080/articles/index
      */
     @GetMapping("/articles")
     public String index(Model model) {
@@ -83,9 +84,9 @@ public class ArticleController {
 
     /**
      * localhost:8080/articles/{id}/edit GET 요청시 데이터 수정 페이지로
-     * @param id
-     * @param model
-     * @return
+     * @param id 해당 article의 id
+     * @param model 저장할 model
+     * @return localhost:8080/articles/{id}/edit
      */
     @GetMapping("/articles/{id}/edit")
     public String edit(@PathVariable Long id, Model model) {
@@ -99,8 +100,8 @@ public class ArticleController {
 
     /**
      * localhost:8080/articles/update POST 요청시 데이터 수정하기
-     * @param form
-     * @return
+     * @param form form 데이터
+     * @return localhost:8080/articles/ + 해당 article의 id
      */
     @PostMapping("/articles/update")
     public String update(ArticleForm form) {
@@ -116,5 +117,24 @@ public class ArticleController {
         }
         //3. 수정 결과 페이지로 리다이렉트하기
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    /**
+     * localhost:8080/articles/delete GET 요청시 데이터 삭제하기
+     * @return
+     */
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다.");
+        //1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        //2. 대상 엔티티 삭제하기
+        if (target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+        //3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
     }
 }
